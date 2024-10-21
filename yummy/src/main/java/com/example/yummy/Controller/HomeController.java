2f1c33fd3d27c769aa998867cf6bb319bdb9b357
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2024-10-20 22:06:14 -0300

    se crean los controladores

--- yummy/src/main/java/com/example/yummy/Controller/HomeController.java
+++ yummy/src/main/java/com/example/yummy/Controller/HomeController.java
@@ -1,23 +1,24 @@
 package com.example.yummy.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @Controller
 public class HomeController {
 
 
     @GetMapping("/home")
-    public String home(@RequestParam(name="name", required = false, defaultValue = "Seguridad y Calidad en el desarrollo")String)
+    public String home(@RequestParam(name="name", required = false, defaultValue = "Seguridad y Calidad en el desarrollo")String name,Model model){
     model.addAttribute("name",name);
-    return "Home";
+    return "Home";}
 
 
     @GetMapping("/")
-    public String root (@RequestParam(name="name", required = false, defaultValue = "Seguridad y Calidad en el desarrollo")Str)
+    public String root (@RequestParam(name="name", required = false, defaultValue = "Seguridad y Calidad en el desarrollo")String name, Model model){
     model.addAttribute("name",name);
-    return "Home";
-
-    
+    return "Home";}
 }
